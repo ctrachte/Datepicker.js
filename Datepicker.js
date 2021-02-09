7d4651db177c45444f086c0a2993ea6f73c2b3f8
Caleb Trachte <cetrachte@hotmail.com>
2021-02-09 11:04:28 -0600

    fixed time pickers to allow 12am times

--- Datepicker.js
+++ Datepicker.js
@@ -690,14 +690,14 @@ class clsDatepicker {
                 this.endHour = this.toMilitary(this.timeElements.endHourValueEl.value)
             }
             if (parseInt(this.timeElements.startHourValueEl.value) === 12 && this.startAmPm === "AM") {
-                this.timeElements.startHourValueEl.value = 1;
+                this.timeElements.startHourValueEl.value = 0;
                 this.timeElements.startHourValueEl.dispatchEvent(new Event('change'));
-                this.startHour = 1;
+                this.startHour = 0;
             }
             if (parseInt(this.timeElements.endHourValueEl.value) === 12 && this.endAmPm === "AM") {
-                this.timeElements.endHourValueEl.value = 1;
+                this.timeElements.endHourValueEl.value = 0;
                 this.timeElements.endHourValueEl.dispatchEvent(new Event('change'));
-                this.endHour = 1;
+                this.endHour = 0;
             }
         }
         if (setProgrammatically) {
