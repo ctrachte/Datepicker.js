3ff3bcbcad12277965377f44167cbfdee93db9c5
Caleb Trachte <cetrachte@hotmail.com>
2021-03-04 13:32:00 -0600

    adjustments to datepicker state for single date dynamic labels

--- Datepicker.js
+++ Datepicker.js
@@ -32,7 +32,7 @@ class clsDatepicker {
         this.leadingTrailingDates = this.options.leadingTrailingDates !== undefined ? this.options.leadingTrailingDates : true;
         this.militaryTime = this.options.militaryTime !== undefined ? this.options.militaryTime : false;
         this.format = this.options.format || (this.timePicker ? (this.militaryTime ? "MM/DD/YYYY HH:mm:ss" : "MM/DD/YYYY hh:mm A") : "MM/DD/YYYY");
-        this.startDateLabel = this.options.startDateLabel !== undefined ? this.options.startDateLabel : "Start Date: ";
+        this.startDateLabel = !this.singleDate ? (this.options.startDateLabel !== undefined ? this.options.startDateLabel : "Start Date: ") : (this.options.startDateLabel !== undefined ? this.options.startDateLabel : "Date: ");
         this.endDateLabel = this.options.endDateLabel !== undefined ? this.options.endDateLabel : "End Date: ";
         this.moment = moment(moment(), this.format, true);
         // methods bound to state context
