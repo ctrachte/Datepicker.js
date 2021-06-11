07574ea340cdd30f012a34e9457c15e5c5816120
Caleb Trachte <cetrachte@hotmail.com>
2021-06-11 12:04:50 -0500

    updated icons, added verbiage for clear

--- Datepicker.js
+++ Datepicker.js
@@ -40,7 +40,7 @@ class Datepicker {
         this.menuOptions = this.options.menuOptions !== undefined ? this.options.menuOptions : [];
         this.autoClose = this.options.autoClose !== undefined ? this.options.autoClose : false;
         this.singleDate = this.options.singleDate !== undefined ? this.options.singleDate : false;
-        this.clearDates = this.options.clearDates !== undefined ? this.options.clearDates : true;
+        this.clearDates = this.options.clearDates !== undefined ? this.options.clearDates : false;
         this.leadingTrailingDates = this.options.leadingTrailingDates !== undefined ? this.options.leadingTrailingDates : true;
         this.militaryTime = this.options.militaryTime !== undefined ? this.options.militaryTime : false;
         this.format = this.options.format || (this.timePicker ? (this.militaryTime ? "MM/DD/YYYY HH:mm:ss" : "MM/DD/YYYY hh:mm A") : "MM/DD/YYYY");
@@ -482,7 +482,6 @@ class Datepicker {
         // cancel dates button:
         let cancelButton = document.createElement('button');
         cancelButton.classList.add("cancelButton");
-        cancelButton.innerHTML = "&#x21BA;";
         cancelButton.type = 'cancel';
         cancelButton.style.gridColumnStart = 1;
         cancelButton.style.gridColumnEnd = 3;
@@ -494,8 +493,11 @@ class Datepicker {
             }
         }.bind(this));
         // TODO: Add conditional styling and text for clearDates=true, and false
-        // clear = red
-        // close = close
+        if(this.clearDates) {
+            cancelButton.innerHTML = "&#x21BA;" +  "    Clear";
+        } else {
+            cancelButton.innerHTML = "&#10006;";
+        }
         calendar.appendChild(cancelButton);
         // submit dates button:
         let submitButton = document.createElement('button');
