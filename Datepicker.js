8a55a5372ae7c5096c3f1cd25460f1314da0cf57
Caleb Trachte <cetrachte@hotmail.com>
2021-06-22 12:04:56 -0500

    moved invocation of `snapTo()` method to prevent UI reset.

--- Datepicker.js
+++ Datepicker.js
@@ -590,16 +590,16 @@ class Datepicker {
                     let onChange = this.onChange;
                     this.onChange = function () { };
                     this.highlightDates();
+                    this.snapTo(this.dates[0], false);
                     if (this.timePicker) {
                         this.setTime(true);
                     } else {
                         this.startDateElement.innerHTML = moment(this.dates[0]).format(this.format);
                         if (!this.singleDate) {
-                                this.endDateElement.innerHTML = moment(this.dates[1]).format(this.format);
+                            this.endDateElement.innerHTML = moment(this.dates[1]).format(this.format);
                         }
                     }
                     this.drawInputElement();
-                    this.snapTo(this.dates[0]);
                     this.closePresetMenu();
                     this.onChange = onChange;
                     this.onChange();
